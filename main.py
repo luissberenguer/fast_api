136cb7677100b9d73db86aa53d43b71df56c4c4e
Luis Berenguer <luis01developer@gmail.com>
2022-04-22 16:21:58 +0200

    queries and path params

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
@@ -31,3 +31,37 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     return person
+
+# Validaciones queries parameters
+
+
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Peron Name",
+        description="This is the person name. It's between 1 and 50 charachters."
+    ),
+    age: str = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's required"
+    )
+):
+    return {name: age}
+
+# Vlidaciones: Path Parameters
+
+
+@app.get('/person/detail/{person_id}')
+def show_person(
+    peron_id: int = Path(
+        ...,
+        gt=0,
+        title="Person Id",
+        description="This is the person id. It's required."
+    )
+):
+    return {peron_id: 'Ese id existe!'}
